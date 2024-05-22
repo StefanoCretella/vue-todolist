c7be144ae641cc446647cc249348470c8e34a0ca
StefanoCretella <cretellastefano213@gmail.com>
2024-05-22 18:38:09 +0200

    [UPD] Added "MILESTONE 2"

--- main.js
+++ main.js
@@ -7,20 +7,23 @@ new Vue({
 
         // Array di to-do
         todos: [
-            { text: 'Fare la spesa', done: false }, // To-do 1
+           { text: 'Fare la spesa', done: false }, // To-do 1
             { text: 'Pulire la casa', done: true }, // To-do 2
             { text: 'Preparare la cena', done: false } // To-do 3
         ]
     },
-    
+
     // Metodi dell'applicazione
     methods: {
 
         // Metodo per cambiare lo stato "done" di un to-do quando viene cliccato
         toggleDone: function(todo) {
-            
-            // Cambia lo stato "done" del to-do (true -> false, false -> true)
-            todo.done = !todo.done;
+            todo.done = !todo.done; // Inverte lo stato di "done"
+        },
+
+        // Metodo per rimuovere un to-do dalla lista
+        removeTodo: function(index) {
+            this.todos.splice(index, 1); // Rimuove il to-do dall'array 
         }
     }
 });
