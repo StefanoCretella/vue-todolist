a435a673e2d2bc041af478e85b54e4d6c8f93639
StefanoCretella <cretellastefano213@gmail.com>
2024-05-22 19:31:09 +0200

    [UPD] Added "MILESTONE 3"

--- main.js
+++ main.js
@@ -1,29 +1,37 @@
 new Vue({
     // Collegamento all'elemento HTML con id "app"
-    el: '#app',
+    el: '#app', 
 
     // Dati dell'applicazione
     data: {
 
         // Array di to-do
         todos: [
-           { text: 'Fare la spesa', done: false }, // To-do 1
+            { text: 'Fare la spesa', done: false }, // To-do 1
             { text: 'Pulire la casa', done: true }, // To-do 2
             { text: 'Preparare la cena', done: false } // To-do 3
-        ]
+        ],
+
+        // Testo del nuovo to-do inserito dall'utente
+        newTodoText: ''
     },
 
     // Metodi dell'applicazione
     methods: {
 
-        // Metodo per cambiare lo stato "done" di un to-do quando viene cliccato
         toggleDone: function(todo) {
-            todo.done = !todo.done; // Inverte lo stato di "done"
+            todo.done = !todo.done; 
         },
 
-        // Metodo per rimuovere un to-do dalla lista
         removeTodo: function(index) {
-            this.todos.splice(index, 1); // Rimuove il to-do dall'array 
+            this.todos.splice(index, 1); 
+        },
+        
+        addTodo: function() {    
+            if (this.newTodoText.trim() !== '') {         
+                this.todos.push({ text: this.newTodoText, done: false });
+                this.newTodoText = '';
+            }
         }
     }
 });
